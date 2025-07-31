6cde1d4b3a54959bcfbc82c63e95074627c3e813
Dale Hobbs <dale.hobbs.dev@gmail.com>
2025-07-30 20:17:55 -0500

    erased test code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,14 +5,6 @@ import { createCorn } from "./seeds/corn.js";
 import { createSoybean } from "./seeds/soybean.js";
 import { createWheat } from "./seeds/wheat.js";
 import { createSunflower } from "./seeds/sunflower.js";
-import { addPlant,usePlants } from "./field.js"
+import { addPlant, usePlants } from "./field.js"
 
 const yearlyPlan = createPlan();
-
-console.log("Welcome to the main module")
-
-const cornSeed = createCorn()
-console.log(cornSeed)
-
-addPlant (cornSeed)
-const duplicateFieldPlants = usePlants ()
